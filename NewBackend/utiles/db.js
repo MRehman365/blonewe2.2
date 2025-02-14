8b5a015825e3bf6a9ae9eaea42c3156d9d74c997
Muhammad Rehman <141804487+MRehman365@users.noreply.github.com>
2025-02-14 11:13:36 +0500

    a

--- NewBackend/utiles/db.js
+++ NewBackend/utiles/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const db = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/BloneWe");
+        await mongoose.connect("mongodb+srv://muhammad_rehman:rehmanislam1@cluster0.4onln.mongodb.net/BloneWe");
         console.log("Database connected successfully");
     } catch (error) {
         console.log("Database connection failed");
