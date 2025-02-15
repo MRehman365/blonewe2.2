ca6e6727de18073a6e94b0ffb8a8db75e954e110
Muhammad Rehman <141804487+MRehman365@users.noreply.github.com>
2025-02-15 16:52:30 +0500

    s added

--- client/src/store/api.js
+++ client/src/store/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
  
 const api = axios.create({
     // baseURL: "http://localhost:8000/api",
-    baseURL: "http://ecoommerce.codemax.live/api",
+    baseURL: "https://ecoommerce.codemax.live/api",
 });
 
 export default api;
